958f568e2087a563d96822acea0d93679d2e77b8
Marco Santos <marco.santos@glartek.com>
2020-08-03 23:09:08 +0100

    feat: add missing SenML functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,14 +6,23 @@ import * as chai from 'chai';
 const expect = chai.expect;
 describe('Validate SenML record', () => {
 
-  it('should be able to validate senML record' , () => {
+  it('should be able to validate senML record', () => {
     let record: SenMLRecord[] = [{
-        Name: "Bananas",
-        Value: 5
+      BaseTime: 100,
+      BaseUnit: "base-unit",
+      BaseVersion: 11,
+      BaseSum: 100,
+      BaseValue: 34,
+      Name: "name",
+      Unit: "unit",
+      Time: 150,
+      UpdateTime: 300,
+      Value: 43.0,
+      Sum: 20.0,
     }]
 
     let packRecord: Pack = {
-        Records: record,
+      Records: record,
     }
 
     expect(Validate(packRecord)).to.equal(null);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 const ErrVersionChange = { name: 'ErrVersionChange', message: 'version change' },
+  ErrUnsuportedFormat = { name: 'ErrUnsupportedFormat', message: 'unsupported format' },
   ErrEmptyName = { name: 'ErrEmptyName', message: 'empty name' },
   ErrTooManyValues = { name: 'ErrTooManyValues', message: 'more than one value in the record' },
   ErrNoValues = { name: 'ErrNoValues', message: 'no value or sum field found' },
   ErrBadChar = { name: 'ErrBadChar', message: 'invalid char' };
+
+const defaultVersion = 10;
 
 // Record represents one senML record.
 export interface SenMLRecord {
@@ -25,6 +28,12 @@ export interface SenMLRecord {
   Sum?: number;
 }
 
+enum Format {
+  JSON = 1,
+  XML,
+  CBOR,
+}
+
 // Pack consists of SenML records array.
 export interface Pack {
   XMLName?: boolean;
@@ -32,11 +41,89 @@ export interface Pack {
   Records: SenMLRecord[];
 }
 
+// Encode takes a SenML Pack and encodes it using the given format.
+export function Decode(msg: string, format: Format): Pack | Error {
+  let p: Pack;
+  switch (format) {
+    case Format.JSON:
+      p = JSON.parse(msg);
+    break;
+    default:
+      return ErrUnsuportedFormat;
+  }
+
+  let err = Validate(p);
+  if (err !== null) return err;
+
+  return p;
+}
+
+// Encode takes a SenML Pack and encodes it using the given format.
+export function Encode(p: Pack, format: Format): string | Error {
+  switch (format) {
+    case Format.JSON:
+      return JSON.stringify(p);
+    default:
+      return ErrUnsuportedFormat;
+  }
+}
+
+// Normalize removes all the base values and expands records values with the base items.
+// The base fields apply to the entries in the Record and also to all Records after
+// it up to, but not including, the next Record that has that same base field.
+export function Normalize(p: Pack): Pack | Error {
+  let err = Validate(p);
+  if (err !== null) {
+    return err;
+  }
+
+  var bname: string | undefined = "";
+  var btime: number | undefined = 0;
+  var bsum: number | undefined = 0;
+  var bunit: string | undefined = "";
+
+  let records = new Array<SenMLRecord>();
+
+  p.Records.forEach(function (r) {
+    if (r.BaseTime != 0) btime = r.BaseTime;
+    if (r.BaseSum != 0) bsum = r.BaseSum;
+    if (r.BaseUnit != "") bunit = r.BaseUnit;
+    if (r.BaseName && r.BaseName.length > 0) bname = r.BaseName;
+    if (r.Time && btime) r.Time = r.Time + btime;
+
+    r.Name = bname + r.Name;
+
+    if (r.Sum && bsum) r.Sum = bsum + r.Sum;
+    if (r.Unit == "" && bunit) r.Unit = bunit;
+    if (r.Value && (r.BaseValue && r.BaseValue != 0)) r.Value = r.BaseValue + r.Value;
+
+    // If the version is default, it must not be present in resolved records.
+    // Validate method takes care that the version is the same on all the records.
+    if (r.BaseVersion == defaultVersion) r.BaseVersion = 0;
+
+    // Remove Base Values from the Record.
+    r.BaseTime = 0
+    r.BaseValue = 0
+    r.BaseUnit = ""
+    r.BaseName = ""
+    r.BaseSum = 0
+
+    // Add to the SenML array
+    records.push(r);
+  });
+
+  p.Records = records;
+
+  // TODO: Need to sort
+
+  return p
+}
+
 // Validate validates SenML records.
 export function Validate(p: Pack): Error | null {
-  var bver: number | undefined;
-  var bname: string | undefined;
-  var bsum: number | undefined;
+  var bver: number | undefined = 0;
+  var bname: string | undefined = "";
+  var bsum: number | undefined = 0;
 
   p.Records.forEach(function (r) {
     // Check if version is the same for all the records.
@@ -59,9 +146,7 @@ export function Validate(p: Pack): Error | null {
     if (r.StringValue != null) valCnt++;
 
     if (valCnt > 1) return ErrTooManyValues;
-
     if (r.Sum != null || bsum != 0) valCnt++;
-
     if (valCnt < 1) return ErrNoValues;
 
     let err = validateName(name);
